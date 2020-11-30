438b83085d34c572746467bb0384b0da7fff52a6
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-30 11:36:51 -0300

    Create Car class

---
+++ MigrationCollection/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MigrateCosmosDBContainers
+{
+    public class Car
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("Wheels")]
+        public int NumberOfWheels { get; set; }
+
+        [JsonPropertyName("Doors")]
+        public int NumberOfDoors { get; set; }
+
+        [JsonPropertyName("UniqueKey")]
+        public string UniqueKey { get; set; }
+    }
+}
