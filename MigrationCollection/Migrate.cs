1c70b3a346f478193fad62922c9e06f6753e105e
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-30 11:37:03 -0300

    Create Migration class

---
+++ MigrationCollection/Migrate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MigrateCosmosDBContainers
+{
+    public class Migrate
+    {
+        public static async Task Migration()
+        {
+            FeedIterator<Car> feedIteratorAccount = Program._container.GetItemLinqQueryable<Car>(false)
+                                                                      .Where(car => car.NumberOfWheels == 4)
+                                                                      .ToFeedIterator();
+
+            while (feedIteratorAccount.HasMoreResults)
+            {
+                foreach (Car car in await feedIteratorAccount.ReadNextAsync())
+                {
+                    car.UniqueKey = car.Id;
+                    await Program._container.CreateItemAsync(car);
+                }
+            }
+        }
+    }
+}
