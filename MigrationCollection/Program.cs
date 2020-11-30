6bf340572c394e2265d89292f53f8673111ee781
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-30 11:58:54 -0300

    update program class

--- MigrationCollection/Program.cs
+++ MigrationCollection/Program.cs
@@ -6,14 +6,17 @@ namespace MigrateCosmosDBContainers
     public class Program
     {
         private const string _databaseName = "Your Database";
-        private const string _containerName = "Your Container";
+        private const string _containerNameFrom = "Your Container From";
+        private const string _containerNameTo = "Your Container To";
         private const string _connectionString = "Your Connection String";
-        internal static readonly Container _container;
+        internal static readonly Container _containerFrom;
+        internal static readonly Container _containerTo;
 
         static Program()
         {
             CosmosClient _cosmosClient = new CosmosClient(_connectionString);
-            _container= _cosmosClient.GetContainer(_databaseName, _containerName);
+            _containerFrom = _cosmosClient.GetContainer(_databaseName, _containerNameFrom);
+            _containerTo = _cosmosClient.GetContainer(_databaseName, _containerNameTo);
         }
 
 
